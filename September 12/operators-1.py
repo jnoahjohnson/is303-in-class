a390acdf60fe7a635856b56ceee6012a6dd17745
Noah Johnson <jnoahjohnson@gmail.com>
2022-09-12 14:02:31 -0600

    New updates - Sep 12

---
+++ September 12/operators-1.py
@@ -0,0 +1,33 @@
+# Description: Operators in python
+# Author: Noah Johnson
+
+num1 = 5
+num2 = 15
+mySum = num1 + num2
+print(mySum)
+
+num1 = 5.2
+num2 = 15
+mySum = num1 + num2
+print(mySum)
+
+myNumber = 90
+print(myNumber % 60)
+
+# Floor Division
+num1 = 20
+num2 = 42
+
+myFloor = num2 // num1
+print(myFloor)
+
+# Rounding
+myNum = 55.5551
+roundedNum = round(myNum, 2)
+print(roundedNum)
+
+myGrade = 90
+myGrade %= 5
+# myGrade = myGrade + 5
+
+print(myGrade)
--- September 12/weather.py
+++ September 12/weather.py
@@ -1,52 +1,52 @@
-# Description: Weather in Python
+# Description: Give ideas of activities based on the weather
 # Author: Noah Johnson
 
-# Ask for the current temperature
-currTemp = int(input("What is the current temperature? "))
+# Please prompt the user for the current temperature outside and convert their input into an integer
+from re import I
 
-# Ask if it is raining
-isRaining = input("Is it raining? (y/n) ")
 
-# Convert isRaining to a boolean
-if isRaining.lower() == "y":
-    isRaining = True
-else:
-    isRaining = False
+currTemp = int(input("What is the temperature outside? "))
 
-# Ask if it is snowing
-isSnowing = input("Is it snowing? (y/n) ")
+# ask the user if it is raining and assign it to the variable isRaining
+    # Make sure to tell the user to type either a y or n for yes or no.
 
-# Convert isSnowing to a boolean
-if isSnowing.lower() == "y":
-    isSnowing = True
-else:
-    isSnowing = False
+isRaining = input("Is it raining (y/n)?")
 
-# Ask if it is windy
-isWindy = input("Is it windy? (y/n) ")
+# Do the same thing for isSnowing 
+isSnowing = input("Is it snowing? ")
 
-# Convert isWindy to a boolean
-if isWindy.lower() == "y":
-    isWindy = True
-else:
-    isWindy = False
+# Do the same thing for isWindy
+isWindy = input("Is it windy? ")
 
-if currTemp >= 80:
-    if isRaining:
-        # Tell the user to do an activity
-        print("Put on boots and dance in the rain")
+# If it is 90 degrees or more, check to see if it is raining. 
+#   If it is, suggest that the user put on rain boots and go jump in puddles. 
+#   If it is not raining, tell the user to go swimming!
+if currTemp >= 90:
+    if isRaining.lower() == "y":
+        print("Go put on rain boots and jump in puddles")
     else:
-        print("Go swimming")
-elif currTemp >= 60:
-    if isSnowing:
-        print("Go skiing")
-    else:
-        print("Go hiking")
-else:
+        print("Go swimming!")
+
+# If it is between 60 and 89 degrees, check to see if it is windy. 
+#   If it is, tell the user to fly a kite. 
+#   If it is not, tell the user to go play spikeball (or another fun outdoor activity).
+isWindy = isWindy.lower() == "y"
+
+# if isWindy.lower() == "y":
+#     isWindy = True
+# else:
+#     isWindy = False
+
+if currTemp >= 60 and currTemp <= 89:
     if isWindy:
-        print("Go kite flying")
-    else:
-        print("Go sledding")
+        print("Fly a kite!")
+    else: 
+        print("Go play spikeball")
 
-
-       
+# If it is less than 60, check to see if it is snowing. 
+#   If it is, tell the user to go sledding. 
+#   If it is not, tell the user to make hot chocolate and play board games (or another fun indoor activity).
+if currTemp < 60 and isSnowing.lower() == "y":
+    print("Go sledding!")
+elif currTemp < 60:
+    print("Make hot chocolate and play board games")
